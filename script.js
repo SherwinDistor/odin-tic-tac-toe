1d3b2c4f93c22f6d7d6b9b12a8741234275a16f1
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-12-16 23:22:13 -0600

    Testing modular patterns

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hello world')
+const Formatter = (function(doc) {
+    console.log('start')
+    
+    const timesRun = []
+
+    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`)
+    // const setTimesRun = () => {
+    //     log('setting times run')
+    //     ++timesRun
+    // }
+
+    const makeUppercase = (text) => {
+        log('making uppercase')
+        // timesRun.push(null)
+        return text.toUpperCase()
+    }
+
+    const writeToDom = (selector, message) => {
+        if (!!doc && 'querySelector' in doc) {
+            doc.querySelector(selector).innerHTML = message
+        }
+    }
+
+    return {
+        makeUppercase,
+        writeToDom,
+    }
+})(document)
+
+console.log(Formatter.makeUppercase('hello'))
+
+// Formatter.timesRun = 10
+// console.log(Formatter.timesRun.length)
+
+Formatter.writeToDom('#target', 'hi there')
