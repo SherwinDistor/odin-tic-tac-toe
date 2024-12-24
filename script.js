a0ba27be45ec840c3402a57b5df17139756f72a1
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-12-24 15:30:59 -0600

    Create array to store gameboard values

--- script.js
+++ script.js
@@ -9,90 +9,20 @@
 // all events can be unbound
 
 
-const Formatter = (function(doc) {
-    console.log('start')
+const Gameboard = (function() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0);
+        }
+    }
+
     
-    const timesRun = []
 
-    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`)
-    // const setTimesRun = () => {
-    //     log('setting times run')
-    //     ++timesRun
-    // }
-
-    const makeUppercase = (text) => {
-        log('making uppercase')
-        // timesRun.push(null)
-        return text.toUpperCase()
-    }
-
-    const writeToDom = (selector, message) => {
-        if (!!doc && 'querySelector' in doc) {
-            doc.querySelector(selector).innerHTML = message
-        }
-    }
-
-    return {
-        makeUppercase,
-        writeToDom,
-    }
-})(document)
-
-// console.log(Formatter.makeUppercase('hello'))
-
-// Formatter.timesRun = 10
-// console.log(Formatter.timesRun.length)
-
-// Formatter.writeToDom('#target', 'hi there')
-
-
-const counter = (function() {
-
-    // private stuff
-    let count = 0;
-
-    function print(message) {
-        console.log(message + '---' + count)
-    }
-
-    // return an object
-    return {
-        //value: count,
-
-        get: function() {
-            return count;
-        },
-
-        set: function(value) {
-            count = value;
-        },
-
-        increment: function() {
-            count += 1;
-            print('After increment: ')
-        },
-
-        reset: function() {
-            print('Before reset: ')
-            count = 0;
-            print('After reset: ')
-        }
-    }
-
+    console.log(board)
 })();
 
-console.log(counter.get());
-
-counter.set(5);
-
-console.log(counter.get());
-
-counter.increment();
-counter.increment();
-counter.increment();
-
-console.log(counter.get());
-
-counter.reset();
-
-console.log(counter.get());
