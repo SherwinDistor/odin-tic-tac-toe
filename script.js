cceddd014b2c0e0503997bdc0370fa8b2b80c9eb
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-01-05 17:26:35 -0600

    Add the display status messages to the ui

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ const Game = (() => {
         currentPlayer = player1;
         Gameboard.clearBoard();
         console.log(`Game started with ${player1.getName()} and ${player2.getName()}`);
+        DisplayController.updateStatus(`Game started with ${player1.getName()} and ${player2.getName()}`);
     }
     
     // Add method to take a turn passing in the row and column where the player picks
@@ -83,12 +84,14 @@ const Game = (() => {
             // Validate turn by checking for a winner first
             if (checkWinner(currentPlayer.getMarker())) {
                 console.log(`${currentPlayer.getName()} wins!`);
+                DisplayController.updateStatus(`${currentPlayer.getName()} wins!`);
                 currentPlayer.incrementWins();
                 return true;
             }
             // Validate turn by checking for a tie
             if (checkTie()) {
                 console.log('It is a tie.');
+                DisplayController.updateStatus('It is a tie.');
                 return true;
             }
             console.log(`${currentPlayer.getName()} took a turn.`)
@@ -104,7 +107,11 @@ const Game = (() => {
     const switchTurn = () => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
         console.log(`It is now ${currentPlayer.getName()}'s turn.`);
+        DisplayController.updateStatus(`It is now ${currentPlayer.getName()}'s turn.`);
     }
+
+    // Add method to get current player
+    const getCurrentPlayer = () => currentPlayer;
 
     // Add method to check winner
     const checkWinner = (marker) => {
@@ -151,7 +158,7 @@ const Game = (() => {
     }
 
     // Make methods available 
-    return { startGame, playTurn, restartGame };
+    return { startGame, playTurn, restartGame, getCurrentPlayer };
 
 })();
 
