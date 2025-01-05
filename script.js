199927f42f2de462aeb36ca91c78f15b0c06ee4c
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-01-05 16:00:29 -0600

    Add comments to code

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-// self-contained module
-// everything to do with my module is in my module
-// no global variables
-// if a module manages more than one thing is should be split up
-// separation of concerns
-// dry code: don't repeat yourself
-// efficient DOM usage
-// very few query selectors
-// all events can be unbound
-
-
+// Create gameboard using module pattern 
 const Gameboard = (function() {
+    // Define gameboard size
     const rows = 3;
     const columns = 3;
     const board = [];
 
+    // Use nested for loop to create a 2D array
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
@@ -21,8 +13,10 @@ const Gameboard = (function() {
         }
     }
 
+    // Add method to get board when requested
     const getBoard = () => board;
 
+    // Add method to update square when user picks a square
     const updateSquare = (row, column, marker) => {
         if (board[row][column] === 0) {
             board[row][column] = marker;
@@ -31,26 +25,32 @@ const Gameboard = (function() {
         return false;
     }
 
+    // Add method to print board to console
     const printBoard = () => {
         console.log(board.map(row => row.join(" ")).join("\n"))
     }
 
+    // Make methods available
     return { getBoard, updateSquare, printBoard }
 
 })();
 
 // Create players factory to hold information
 const Player = (name, marker) => {
+    // Initialize wins to keep track of players win total
     let wins = 0;
 
+    // Add methods to get players name, marker, and win total
     const getName = () => name;
     const getMarker = () => marker;
     const getWins = () => wins;
 
+    // Add method to increase win total
     const incrementWins = () => {
         wins++;
     };
 
+    // Make methods available
     return { getName, getMarker, getWins, incrementWins }
 }
 
@@ -59,19 +59,23 @@ const Game = (() => {
     // Initialize players
     let player1, player2, currentPlayer;
 
+    // Add method to start game and set player 1 as current player and assign marker
     const startGame = (name1, name2) => {
-        player1 = Player(name1, 1);
-        player2 = Player(name2, 2);
+        player1 = Player(name1, 'X');
+        player2 = Player(name2, 'O');
         currentPlayer = player1;
     }
     
+    // Add method to take a turn passing in the row and column where the player picks
     const playTurn = (row, column) => {
         if (Gameboard.updateSquare(row, column, currentPlayer.getMarker())) {
+            // Validate turn by checking for a winner first
             if (checkWinner(currentPlayer.getMarker())) {
                 console.log(`${currentPlayer.getName()} wins!`);
                 currentPlayer.incrementWins();
                 return true;
             }
+            // Validate turn by checking for a tie
             if (checkTie()) {
                 console.log('It is a tie.');
                 return true;
@@ -83,11 +87,13 @@ const Game = (() => {
         return false;
     }
 
+    // Add method to switch players turn
     const switchTurn = () => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
         console.log(`It is now ${currentPlayer.getName()}'s turn.`);
     }
 
+    // Add method to check winner
     const checkWinner = (marker) => {
         const board = Gameboard.getBoard();
 
@@ -118,35 +124,14 @@ const Game = (() => {
 
     }
 
+    // Add method to check for a tie
     const checkTie = () => {
         const board = Gameboard.getBoard();
         return board.flat().every((space) => space !== 0);
     }
 
+    // Make methods available 
     return { startGame, playTurn }
 
 })();
 
-Gameboard.printBoard()
-
-Game.startGame("Henry", "Peter")
-
-Game.playTurn(1, 1);
-
-Gameboard.printBoard()
-
-Game.playTurn(0, 1);
-
-Gameboard.printBoard()
-
-Game.playTurn(0, 2)
-
-Gameboard.printBoard()
-
-Game.playTurn(2, 2)
-
-Gameboard.printBoard()
-
-Game.playTurn(2, 0)
-
-Gameboard.printBoard()
